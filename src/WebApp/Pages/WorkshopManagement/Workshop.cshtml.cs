cc1a226828592b593f84bdb2e05d1db5b4968553
Damith Dias <damith.dias@austal.com>
2022-10-09 09:04:55 +0530

    add react project

--- src/WebApp/Pages/WorkshopManagement/Workshop.cshtml.cs
+++ src/WebApp/Pages/WorkshopManagement/Workshop.cshtml.cs
@@ -5,15 +5,34 @@ namespace WebApp.Pages;
 
 public class WorkshopModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<WorkshopModel> _logger;
 
-    public WorkshopModel(ILogger<IndexModel> logger)
+    public WorkshopModel(ILogger<WorkshopModel> logger)
     {
         _logger = logger;
     }
 
     public void OnGet()
     {
-
+      
     }
+    [BindProperty]
+    public List<WorkShop>? WorkShops { get; set; } = new List<WorkShop>
+    {
+        new WorkShop
+        {
+            Vehicle = "Jeep",
+            Description = "4dr"
+        }
+    };
+}
+public class WorkShop
+{
+
+    public string Id { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public string StartTime { get; set; } = string.Empty;
+    public string EndTime { get; set; } = string.Empty;
+    public string Vehicle { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
